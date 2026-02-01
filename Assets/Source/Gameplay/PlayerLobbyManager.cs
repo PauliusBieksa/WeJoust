95f717795c86abb1f878b0f6e1d6db41780ce4f1
Paulius Bieksa <pauliuspaulius@hotmail.com>
2026-02-01 14:52:11 +0000

    sadasd

--- Assets/Source/Gameplay/PlayerLobbyManager.cs
+++ Assets/Source/Gameplay/PlayerLobbyManager.cs
@@ -12,7 +12,8 @@ public class PlayerLobbyManager : MonoBehaviour
    public List<Color> colors = new List<Color>();
    public PlayerInputManager playerInputManager;
    public Image WinScreen;
-   
+
+    InputAction interactAction;
    
    public static Action<Player> RegisterPlayerEvent;
    public static Action<Player> UnregisterPlayerEvent;
@@ -28,7 +29,13 @@ public class PlayerLobbyManager : MonoBehaviour
       Time.timeScale = 0;
    }
 
-   private void Update()
+    private void Start()
+    {
+
+        interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    private void Update()
    {
       LobbyJoining();
       Winner();
@@ -61,6 +68,10 @@ public class PlayerLobbyManager : MonoBehaviour
                winner.playerCard.GetComponent<RectTransform>().anchoredPosition = new Vector2(1920, 1080);
                winner.playerCard.GetComponent<RectTransform>().position = new Vector2(0f, 0f);
             }
+         }
+         if (interactAction.triggered)
+         {
+                Restart();
          }
       }
    }
@@ -130,7 +141,7 @@ public class PlayerLobbyManager : MonoBehaviour
    }
    
    //restart
-   void OnAnyButton(InputControl control)
+   void Restart()
    {
       if(winner != null)
       {
